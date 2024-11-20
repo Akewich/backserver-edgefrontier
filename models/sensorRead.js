a83c029aea33c5b34cd6a03118f6c7a39c720a3f
Akewich <ekawichgm@gmail.com>
2024-11-20 17:45:03 +0700

    Update sensorRead.js

--- models/sensorRead.js
+++ models/sensorRead.js
@@ -1,15 +1,14 @@
 const { default: mongoose } = require("mongoose");
 
 const sensorDataSchema = new mongoose.Schema({
-    SensorName: String,
-    Carbon: Number,
-    VOC: Number,
-    Radon: Number,
-    Temperature: Number,
-    Humidity: Number,
-    Pressure: Number,
-    updateAt: {type : Date, default: Date.now}
+  SensorName: String,
+  CO2: Number,
+  VOC: Number,
+  Radon: Number,
+  Temperature: Number,
+  Humidity: Number,
+  Pressure: Number,
+  updateAt: { type: Date, default: Date.now },
+});
 
-})
-
-module.exports = mongoose.model('sensorReading', sensorDataSchema)
+module.exports = mongoose.model("sensorReading", sensorDataSchema);
